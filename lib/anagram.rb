ca4d982e9805ce4825b4fdc74c2f8d65f97a089e
Martin Kamau <martinkimu09@gmail.com>
2022-11-16 03:41:58 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :listen
+
+    def initialize(listen)
+        @listen = listen
+    end
+
+    def match(array)
+        sorted_word = @listen.chars.sort.join
+        array.select do |w|
+            w.chars.sort.join == sorted_word
+        end
+    end
+end
